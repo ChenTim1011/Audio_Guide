de1ce27ef44ae7896e9b2b8251cc633f11b8825f
TimChen1011 <110208059@g.nccu.edu.tw>
2024-04-25 12:52:57 +0800

    Adjust ip setting and back to localhost.

--- public/chooseRole.js
+++ public/chooseRole.js
@@ -11,10 +11,8 @@ document.getElementById('viewer-button').addEventListener('click', function() {
 document.getElementById('submit-password').addEventListener('click', function() {
     const password = document.getElementById('password').value;
     if (password === "12345") {
-        fetch('/get-ip').then(response => response.json()).then(data => {
-            const serverIP = data.ip;
-            window.location.href = `http://raspberrypi.local:5000/host.html`;
-        });
+            window.location.href = `http://localhost:5000/host.html`;
+        
     } else {
         alert('密碼錯誤');
     }
@@ -22,8 +20,5 @@ document.getElementById('submit-password').addEventListener('click', function() 
 
 document.getElementById('submit-name').addEventListener('click', function() {
     const name = document.getElementById('name').value;
-    fetch('/get-ip').then(response => response.json()).then(data => {
-        const serverIP = data.ip;
-        window.location.href = `http://raspberrypi.local:5000/viewer.html?name=` + encodeURIComponent(name);
-    });
+        window.location.href = `http://localhost:5000/viewer.html?name=` + encodeURIComponent(name);
 });
